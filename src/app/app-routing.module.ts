cfb20a342f08bd43e868abf3944ff059e910ed20
zakyfa <darkzaky68@gmail.com>
2021-10-29 22:39:36 +0700

    game-ku uts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TournamentComponent } from './tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile',component:ProfileComponent},
+  {path:'tournament',component:TournamentComponent},
+  {path:'leaderboard',component:LeaderboardComponent},
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
